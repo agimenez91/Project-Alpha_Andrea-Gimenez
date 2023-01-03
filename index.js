3ad492f4220b057030401acb4c7da6e0e9e991d2
agimenez91 <andrea.gimenez91@gmail.com>
2023-01-03 20:13:52 +0100

    dark mode added by JS

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-alert ("hola");
+function toggleStylesheet() {
+    var currentStylesheet = document.getElementById("stylesheet");
+    var currentLinkText = document.getElementById("dark-link").textContent;
+    if (currentStylesheet.getAttribute("href") === "style.css") {
+      currentStylesheet.setAttribute("href", "stylesheetdark.css");
+      document.getElementById("dark-link").textContent = "Light";
+    } else {
+      currentStylesheet.setAttribute("href", "style.css");
+      document.getElementById("dark-link").textContent = "Dark";
+    }
+  }
+  
