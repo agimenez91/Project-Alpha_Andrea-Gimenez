12b6723c9e5f7bad2ecb133e41f5a8c77b6abffb
agimenez91 <andrea.gimenez91@gmail.com>
2023-01-10 10:39:05 +0100

    personal page added

--- index.js
+++ index.js
@@ -10,3 +10,17 @@ function toggleStylesheet() {
     }
   }
   
+
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+  hamburger.classList.toggle('active');
+});
+
+navMenu.addEventListener('click', () => {
+  navMenu.classList.remove('active');
+  hamburger.classList.remove('active');
+});
+  
