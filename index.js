17c3272f450052e0bc331ade227cfdb86c4a0a1b
agimenez91 <andrea.gimenez91@gmail.com>
2023-06-10 16:05:40 +0200

    routes fixed

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ function toggleStylesheet() {
     if (currentStylesheet.getAttribute("href") === "style.css") {
       currentStylesheet.setAttribute("href", "stylesheetdark.css");
       document.getElementById("dark-link").textContent = "Light";
-      document.getElementById("coverPic").src="Images/FotoAndreaGiménezDark.png";
+      document.getElementById("coverPic").src="/Images/FotoAndreaGiménezDark.png.png";
     } else {
       currentStylesheet.setAttribute("href", "style.css");
       document.getElementById("dark-link").textContent = "Dark";
-      document.getElementById("coverPic").src="Images/FotoAndreaGiménezLight.png";
+      document.getElementById("coverPic").src="/Images/FotoAndreaGiménezLight.png";
     }
   }
   
